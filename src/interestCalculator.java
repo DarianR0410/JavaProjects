7a9f701d82e3aec988f506e52e5f2fc44a1f855d
Darian Ramos <ram.darian0410@gmail.com>
2025-02-03 12:34:19 -0400

    I created a different calculator applying the interest compounded formula in Java

---
+++ src/interestCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class interestCalculator {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double principal;
+		double rate;
+		int timesCompunded;
+		int years;
+		double amount;
+		
+		System.out.print("Enter the principal amount: ");
+		principal = scanner.nextDouble();
+		
+		System.out.print("Enter the interest rate (in %): ");
+		rate = scanner.nextDouble() / 100;
+		
+		System.out.print("Enter the # of times compunded: ");
+		timesCompunded = scanner.nextInt();
+		
+		System.out.print("Enter the amount of years: ");
+		years = scanner.nextInt();
+		
+		amount = principal * Math.pow(1 + rate / timesCompunded, timesCompunded * years);
+		
+		System.out.printf("The amount after %d years is $%.2f", years, amount);
+		
+		scanner.close();
+
+	}
+
+}
