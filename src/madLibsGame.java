155b403231f14422e2e5ecb08c6435eaa48294aa
DarianR0410 <ram.darian0410@gmail.com>
2025-02-04 13:18:24 -0400

    Error solved in this commit

--- src/madLibsGame.java
+++ src/madLibsGame.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
 
 public class madLibsGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Scanner scanner = new Scanner(System.in);
+		
+		String adjetiveOne;
+		String nounOne;
+		String adjetiveTwo;
+		String verbOne;
+		String adjetiveThree;
+
+		System.out.println("\nEnter an adjetive: ");
+		adjetiveOne = scanner.nextLine();
+		System.out.println("Enter a noun (animal or person): ");
+		nounOne = scanner.nextLine();
+		System.out.println("Enter an adjetive:");
+		adjetiveTwo = scanner.nextLine (); 
+		System.out.println("Enter a verb ending with -ing:");
+		verbOne = scanner.nextLine();
+		System.out.println("Enter an adjetive:");
+		adjetiveThree = scanner.nextLine();
+
+		
+		System.out.println("Today I went to a "+ adjetiveOne + " zoo");
+		System.out.println("In an exhibit, I saw a "+ nounOne + ".");
+		System.out.println(nounOne + " was "+ adjetiveTwo + " and " + verbOne + "!");
+		System.out.println("I was " + adjetiveThree + "!");
+		
+		scanner.close();
 
 	}
 
